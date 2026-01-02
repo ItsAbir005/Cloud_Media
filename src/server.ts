4c229449e1ce3a89eb628daef33c4ba3dedda64d
Abir Maity <abirmaity44@gmail.com>
2026-01-02 09:38:06 +0530

    add route

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,19 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import { globalError } from "./middlewares/globalError";
+import { testPostgres } from "./db/postgres";
+import { connectMongo } from "./db/mongo";
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.get("/health", async (req, res, next) => {
+  try {
+    await testPostgres();
+    await connectMongo();
+    res.json({ success: true, message: "All services healthy" });
+  } catch (err) {
+    next(err);
+  }
+});
+app.use(globalError);
 app.listen(8080, () => console.log("Server running on 8080"));
